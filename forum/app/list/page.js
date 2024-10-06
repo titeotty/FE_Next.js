cd017cb38c771734d17e23886116cd95dde3b8eb
HYEON <titeotty12@gmail.com>
2024-10-06 11:52:20 +0900

    10062

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -5,7 +5,10 @@ export default async function List(){
     const db = (await connectDB).db("forum")
     let result = await db.collection('post').find().toArray()
     console.log(result)
-
+    result = result.map((a) => {
+      a._id = a._id.toString();
+      return a;
+    });
     return (
       <div className="list-bg">
         <ListItem result={result} />
