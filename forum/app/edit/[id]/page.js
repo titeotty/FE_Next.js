b035b288bf48cc92e1f760726f60e841b8111a8a
HYEON <titeotty12@gmail.com>
2024-10-06 11:15:14 +0900

    1006

--- forum/app/edit/[id]/page.js
+++ forum/app/edit/[id]/page.js
@@ -1,22 +1,21 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 
-export default async function handler(요청, 응답) {
-  if(요청.method == 'POST'){
-    console.log(요청.body)
-  }
-  const db = (await connectDB).db("forum")
-  let result = await db.collection('post').updateOne(
-    {_id : new ObjectId(props.params.id)}
-  )
-  return (
-<div className="write">
-  <form action="/api/post/edit" method="POST">
-    <input name="title" defaultValue={result.title}/>
-    <input name="content" defaultValue={result.content}/>
-    <input name="_id" defaultValue={result._id.toString()}/>
-    <button type="submit">전송</button>
-  </form>
-</div> 
-  )
-} 
+export default async function Edit(props) {
+    const db = (await connectDB).db("forum");
+    let result = await db.collection("post").findOne({ _id: new ObjectId(props.params.id) });
+
+    await db.collection("post").updateOne({}, { $set: {} });
+
+    return (
+        <div className="p-20">
+            <h4>수정</h4>
+            <form action="/api/post/edit" method="POST">
+                <input name="title" defaultValue={result.title} />
+                <input name="content" defaultValue={result.content} />
+                <input name="_id" defaultValue={result._id.toString()} style={{ display: "none" }} />
+                <button type="submit">수정</button>
+            </form>
+        </div>
+    );
+}
--- forum/pages/api/post/edit.js
+++ forum/pages/api/post/edit.js
@@ -1,17 +1,14 @@
-import { connectDB } from "@/util/database.js"
+import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 
-export default async function handler(요청, 응답) {
-  if (요청.method == 'POST'){
-
-    let 바꿀거 = {title : 요청.body.title, content : 요청.body.content}
-    let db = (await connectDB).db('forum')
-    let result = await db.collection('post').updateOne(
-      {_id : new ObjectId(요청.body._id)}, 
-      { $set : 바꿀거} 
-    );
-    console.log(result)
-
-    응답.redirect(302, '/list')
-  }
+export default async function handler(req, res) {
+    if (req.method == "POST") {
+        let changeT = {
+            title: req.body.title,
+            content: req.body.content,
+        };
+        const db = (await connectDB).db("forum");
+        let result = await db.collection("post").updateOne({ _id: new ObjectId(req.body._id) }, { $set: changeT });
+        res.redirect(302, "/list");
+    }
 }
